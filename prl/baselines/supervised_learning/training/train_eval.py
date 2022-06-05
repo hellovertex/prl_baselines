3fbb0e44a7d56cfe645dd2882be51edf8431afc3
hellovertex <hellovertex@outlook.com>
2022-06-05 17:52:07 +0200

    added confusion matrix to tensorboard writer

--- prl/baselines/supervised_learning/training/train_eval.py
+++ prl/baselines/supervised_learning/training/train_eval.py
@@ -2,10 +2,15 @@ import logging
 import time
 from collections import OrderedDict
 
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
 import numpy as np
 import torch
 import torch.nn.functional as F
 from prefetch_generator import BackgroundGenerator
+from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
@@ -13,6 +18,7 @@ from prl.baselines.supervised_learning.models.model import Net
 from prl.baselines.supervised_learning.training.dataset import OutOfMemoryDataset
 
 BATCH_SIZE = 1000
+TEST_BATCH_SIZE = 10000
 
 
 def load_checkpoint(path_to_checkpoint):
@@ -22,6 +28,9 @@ def load_checkpoint(path_to_checkpoint):
 def run_train_eval(input_dir,
                    epochs,
                    lr,
+                   batch_size=BATCH_SIZE,
+                   test_batch_size=TEST_BATCH_SIZE,
+                   log_interval=100,  # log training metrics every `log_interval` batches
                    ckpt_interval=10000,  # save checkpoint each `ckpt_interval` batches
                    eval_interval=10000,  # eval each `eval_interval` batches
                    ckpt_dir='./ckpt',
@@ -42,12 +51,15 @@ def run_train_eval(input_dir,
     torch.cuda.manual_seed(1)
 
     # datasets tr te
+    classes = [0, 1, 2, 3, 4, 5]
     dataset = OutOfMemoryDataset(input_dir, batch_size=BATCH_SIZE)
-    testset = OutOfMemoryDataset(input_dir+'/test', batch_size=BATCH_SIZE)
+    testset = OutOfMemoryDataset(input_dir + '/test', batch_size=BATCH_SIZE)
 
     # network
     hidden_dim = [512, 512]
-    net = Net(564, 6, hidden_dim)
+    output_dim = 6
+    input_dim = 564
+    net = Net(input_dim, output_dim, hidden_dim)
 
     # if running on GPU and we want to use cuda move model there
     use_cuda = torch.cuda.is_available()
@@ -76,6 +88,10 @@ def run_train_eval(input_dir,
     # typically we use tensorboardX to keep track of experiments
     writer = SummaryWriter(log_dir=log_dir)
     n_iter = start_n_iter
+    i_train = 0
+    total_loss = 0
+    correct = 0
+
     pbar = tqdm(enumerate(BackgroundGenerator(dataset)), total=len(dataset) / BATCH_SIZE)
     for epoch in range(start_epoch, epochs):
         pbar.set_description(
@@ -83,24 +99,31 @@ def run_train_eval(input_dir,
         start_time = time.time()
         for i, data in pbar:
             # todo convert to pytorch tensors if applicable:
-            label = data.pop['label']
+            labels = data.pop['labels']
             if use_cuda:
                 data = data.cuda()
-                label = label.cuda()
+                labels = labels.cuda()
             prepare_time = start_time - time.time()
 
             # forward and backward pass
             optim.zero_grad()
             output = net(data)
             # todo check if this works with our batches
-            loss = F.cross_entropy(output, label)
-            loss.backward()
+            loss = F.cross_entropy(output, labels)
             loss.backward()
             optim.step()
-
+            total_loss += loss.data.item()
             # udpate tensorboardX
-            # todo: scalar_value
-            writer.add_scalar(tag='', scalar_value=0, global_step=n_iter)
+            pred = output.data.max(1)[1]  # get the index of the max log-probability
+            correct += pred.eq(labels.data).cpu().sum().item()
+            i_train += 1
+            if i % log_interval == 0:
+                writer.add_scalar(tag='Training Loss', scalar_value=total_loss/i_train, global_step=n_iter)
+                writer.add_scalar(tag='Training Accuracy', scalar_value=100.0 * correct / i_train, global_step=n_iter)
+
+                i_train = 0
+                correct = 0
+                total_loss = 0
 
             # compute computation time and *compute_efficiency*
             process_time = start_time - time.time() - prepare_time
@@ -109,7 +132,7 @@ def run_train_eval(input_dir,
             start_time = time.time()
 
             # evaluate
-            if eval_interval % i == 0:
+            if i % eval_interval == 0:
                 # bring models to evaluation mode
                 net.eval()
                 pbar_test = tqdm(enumerate(BackgroundGenerator(dataset)), total=len(testset) / BATCH_SIZE)
@@ -118,16 +141,16 @@ def run_train_eval(input_dir,
                 correct = 0
                 with torch.no_grad():
                     for j, data in pbar_test:
-                        label = data.pop['label']
+                        labels = data.pop['labels']
                         if use_cuda:
                             data = data.cuda()
-                            label = label.cuda()
+                            labels = labels.cuda()
                         output = net(data)
                         test_loss += F.cross_entropy(
-                            output, label, reduction="sum"
+                            output, labels, reduction="sum"
                         ).data.item()  # sum up batch loss
                         pred = output.data.max(1)[1]  # get the index of the max log-probability
-                        correct += pred.eq(label.data).cpu().sum().item()
+                        correct += pred.eq(labels.data).cpu().sum().item()
 
                 test_loss /= len(testset)
                 test_accuracy = 100.0 * correct / len(testset)
@@ -138,14 +161,30 @@ def run_train_eval(input_dir,
                 )
                 # return model to training mode
                 net.train()
-                # todo: write test metrics to tensorboard summarywriter
-                writer.add_scalar(tag='', scalar_value=0, global_step=n_iter)
+
+                # write metrics to tensorboard
+                writer.add_scalar(tag='Test Loss', scalar_value=test_loss, global_step=n_iter)
+                writer.add_scalar(tag='Test Accuracy', scalar_value=test_accuracy, global_step=n_iter)
+
+                # write layer histograms to tensorboard
+                k = 1
+                for layer in net.children():
+                    if isinstance(layer, nn.Linear):
+                        writer.add_histogram(f"layer{k}.weights", layer.state_dict()['weight'], global_step=n_iter)
+                        writer.add_histogram(f"layer{k}.bias", layer.state_dict()['bias'], global_step=n_iter)
+                        k += 1
+
+                # Write confusion matrix to tensorboard
+                cf_matrix = confusion_matrix(labels, output)
+                df_cm = pd.DataFrame(cf_matrix / np.sum(cf_matrix) * 10, index=[i for i in range(output_dim)],
+                                     columns=[i for i in classes])
+                plt.figure(figsize=(12, 7))
+                writer.add_figure("Confusion matrix", sn.heatmap(df_cm, annot=True).get_figure(), epoch)
+
             # save checkpoint if needed
-            if ckpt_interval % i == 0:
+            if i % ckpt_interval == 0:
                 torch.save({'epoch': epoch,
                             'net': net.state_dict(),
                             'n_iter': n_iter,
                             'optim': optim.state_dict(),
                             'loss': loss}, ckpt_dir)  # net
-
-
