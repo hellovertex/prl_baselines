bdd26c65075c7199aa18380e80eb3dd942d7d9b8
sascha <hellovertex@outlook.com>
2022-08-07 16:55:47 +0200

    added unzipping script

---
+++ prl/baselines/utils/unzip_recursively.py
@@ -0,0 +1,40 @@
+"""Use this on virutal machines to get the BulkHands.zip from google drive.
+Useful before running eda.py"""
+import glob
+import io
+import os
+import zipfile
+
+import click
+
+
+def extract(filename, out_dir):
+    z = zipfile.ZipFile(filename)
+    for f in z.namelist():
+        try:
+            os.mkdir(out_dir)
+        except FileExistsError:
+            pass
+        # read inner zip file into bytes buffer
+        content = io.BytesIO(z.read(f))
+        zip_file = zipfile.ZipFile(content)
+        for i in zip_file.namelist():
+            zip_file.extract(i, out_dir)
+
+
+@click.command()
+@click.option("--path_to_zipfile",
+              default="",
+              type=str,
+              help="Absolute path ending with .zip")
+@click.option("--abs_out_path",
+              default="",
+              type=str,  # absolute path
+              help="Absolute path where .zip file should be unzipped to.")
+def main(path_to_zipfile, abs_out_path):
+    zipfiles = glob.glob(path_to_zipfile.__str__() + '/*.zip', recursive=False)
+    [extract(zipfile, out_dir=abs_out_path) for zipfile in zipfiles]
+
+
+if __name__ == '__main__':
+    main()
