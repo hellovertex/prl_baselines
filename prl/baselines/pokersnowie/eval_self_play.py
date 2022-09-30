aa3f9d4bb10aba7e616b440ea7012549cf5496da
sascha <hellovertex@outlook.com>
2022-10-01 01:01:59 +0200

    added Agent and Runner class stubs

--- prl/baselines/pokersnowie/eval_self_play.py
+++ prl/baselines/pokersnowie/eval_self_play.py
@@ -8,21 +8,45 @@ from prl.baselines.pokersnowie.eighteighteight import EightEightEightConverter
 from prl.baselines.supervised_learning.data_acquisition.core.parser import PokerEpisode
 
 
+class Agent:
+    def __init__(self):
+        self._model = None
+        self._card_evaluator = None
+        self._policy = None
+
+    def load_model(self):
+        pass
+
+
 def create_wrapped_environment():
-    pass
+    wrapped_env = False
+    return wrapped_env
 
 
-def load_model():
-    pass
+# Consider using this:
+# class Runner:
+#     def __init__(self):
+#         self._agent = None
+#         self._env = None
+#         self._converter = None
+#
+#     def reset(self):
+#         self._agent = Agent()  # is stateless
+#         self._env = create_wrapped_environment()
+#         self._converter = EightEightEightConverter()
 
 
 def run_games(n_episodes=100):
+    # setup
+    agent = Agent()  # is stateless
+    env = create_wrapped_environment()
     converter = EightEightEightConverter()
     converted = []
-    # episode = init_poker_episode()
-    # converter.from_poker_episode()
-    done = False
+    # env_config = None
     for i in range(n_episodes):
+        # obs, _, _, _ = env.reset(env_config)
+        done = False
+        # episode = init_poker_episode(env_config, obs)
         while not done:
             # todo query agent that queries model [card_eval + fold_prob]
             # todo update_poker_episode
