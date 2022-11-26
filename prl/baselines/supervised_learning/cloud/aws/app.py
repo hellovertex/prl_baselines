011c8e9d893b23d5366c125ac774b83a48e393ef
sascha <hellovertex@outlook.com>
2022-11-26 22:19:46 +0100

    wip sync before hard reset

---
+++ prl/baselines/supervised_learning/cloud/aws/app.py
@@ -0,0 +1,23 @@
+import json
+import torch
+
+
+def lambda_handler(event, context):
+    model = torch.load("tmp/model.pt", map_location=torch.device('cpu'))
+    model.eval()
+    print("## EVENT ")
+    print(event)
+
+    print("## CONTEXT")
+    print(context)
+    obs = json.loads(event['body'])['query']  # list[float]
+    action = torch.argmax(model(torch.Tensor(obs)))
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'default': 'Hello from Lambda!',
+            'action': action.item()  # integer action corresponding to discretized action
+        })
+    }
