fee49e8948d2253833779177def9b7028cedea41
sascha <hellovertex@outlook.com>
2022-12-10 17:20:13 +0100

    Need obs to poker episode converter for baseline evaluation --next step: renormalize blinds from byte vector

---
+++ prl/baselines/utils/obs_vector_to_poker_episode.py
@@ -0,0 +1,8 @@
+from prl.environment.Wrappers.augment import AugmentedObservationFeatureColumns
+
+# use vectorized representation of AugmentedObservationFeatureColumns to parse back to PokerEpisode object
+
+def convert():
+    # PokerEpisode.num_players -- positive stacks
+    # PokerEpisode.blinds -- normalization_sum * sb, bb index
+    pass
