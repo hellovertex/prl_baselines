f8146397d4f5f4f1e59b3eef194745ae92e82ec3
sascha <hellovertex@outlook.com>
2022-12-11 14:53:24 +0100

    upd sync

--- prl/baselines/utils/obs_vector_to_poker_episode.py
+++ prl/baselines/utils/obs_vector_to_poker_episode.py
@@ -1,9 +1,21 @@
-from prl.environment.Wrappers.augment import AugmentedObservationFeatureColumns
+from datetime import datetime
+from typing import Union
 
+import numpy as np
+from prl.environment.Wrappers.augment import AugmentedObservationFeatureColumns
+from prl.baselines.supervised_learning.data_acquisition.core.parser import PokerEpisode
 # use vectorized representation of AugmentedObservationFeatureColumns to parse back to PokerEpisode object
 
-def convert():
+
+def obs_vec_to_episode(obs: Union[list, np.ndarray],
+                       normalization_sum: int) -> PokerEpisode:
+    # PokerEpisode.date: dateime.now()
     # PokerEpisode.num_players -- positive stacks
     # PokerEpisode.blinds -- normalization_sum * sb, bb index
 
+    date = str(datetime.now())
+    hand_id = -1
+    variant = "HUNL"
+    currency_symbol = "$"
+    num_players = None
     pass
