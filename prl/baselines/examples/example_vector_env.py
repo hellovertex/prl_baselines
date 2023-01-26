028276914cd7c8d5faf46539c170e3eec30d7cc5
sascha <hellovertex@outlook.com>
2023-01-26 01:51:05 +0100

    tianshou aec env 5 -- working -- done MC trigger to parallelize action sampling

--- prl/baselines/examples/example_vector_env.py
+++ prl/baselines/examples/example_vector_env.py
@@ -1,6 +1,8 @@
+import os
 from enum import IntEnum
 from functools import partial
-from typing import Optional, Union, List
+from random import random
+from typing import Optional, Union, List, Tuple, Any, Dict
 
 import gym
 import numpy as np
@@ -12,11 +14,32 @@ from pettingzoo.utils.env import ObsType
 from prl.environment.Wrappers.augment import AugmentObservationWrapper
 from prl.environment.Wrappers.base import ActionSpace
 from prl.environment.Wrappers.utils import init_wrapped_env
-from tianshou.data import Collector
+from tianshou.data import Collector, Batch
 from tianshou.env.pettingzoo_env import PettingZooEnv
 from tianshou.env.venvs import SubprocVectorEnv
-from tianshou.policy import MultiAgentPolicyManager, RainbowPolicy
+from tianshou.policy import MultiAgentPolicyManager, RainbowPolicy, BasePolicy
 from tianshou.utils.net.common import Net
+
+from prl.baselines.cpp_hand_evaluator.monte_carlo import HandEvaluator_MonteCarlo
+from prl.baselines.cpp_hand_evaluator.rank import dict_str_to_sk
+from prl.baselines.supervised_learning.models.nn_model import MLP
+
+IDX_C0_0 = 167  # feature_names.index('0th_player_card_0_rank_0')
+IDX_C0_1 = 184  # feature_names.index('0th_player_card_1_rank_0')
+IDX_C1_0 = 184  # feature_names.index('0th_player_card_1_rank_0')
+IDX_C1_1 = 201  # feature_names.index('1th_player_card_0_rank_0')
+IDX_BOARD_START = 82  #
+IDX_BOARD_END = 167  #
+CARD_BITS_TO_STR = np.array(['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A', 'h', 'd', 's', 'c'])
+BOARD_BITS_TO_STR = np.array(['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
+                              'h', 'd', 's', 'c', '2', '3', '4', '5', '6', '7', '8', '9', 'T',
+                              'J', 'Q', 'K', 'A', 'h', 'd', 's', 'c', '2', '3', '4', '5', '6',
+                              '7', '8', '9', 'T', 'J', 'Q', 'K', 'A', 'h', 'd', 's', 'c', '2',
+                              '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A', 'h',
+                              'd', 's', 'c', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J',
+                              'Q', 'K', 'A', 'h', 'd', 's', 'c'])
+RANK = 0
+SUITE = 1
 
 
 # todo implement this https://pettingzoo.farama.org/tutorials/tianshou/intermediate/
@@ -38,7 +61,7 @@ class MultiAgentActionFlags(IntEnum):
     setting num_envs equal to the number of CPU cores available, e.g. 32.
     And can run 32 MC simulations at the same time -- the same number of env.step() we can call at
     the same time."""
-    TriggerMC = 0
+    TriggerMC = 99
 
 
 class MCAgent:
@@ -62,6 +85,10 @@ class TianshouEnvWrapper(AECEnv):
         self.agents = agents
         self.possible_agents = self.agents[:]
         self.env_wrapped = env
+        self._card_evaluator = HandEvaluator_MonteCarlo()
+        self._mc_iters = 5000
+        self._model = self.load_model()
+        self.tightness = .9  # todo make configurable
         # moved this to prl.baselines because I understand we need access to the baseline agents
         # which are not available from within prl_environment
         self._mc_agent = None
@@ -140,17 +167,80 @@ class TianshouEnvWrapper(AECEnv):
         self.next_legal_moves = legal_moves
         self._last_obs = obs
 
+    def card_bit_mask_to_int(self, c0: np.array, c1: np.array, board_mask: np.array) -> Tuple[List[int], List[int]]:
+        c0_1d = dict_str_to_sk[CARD_BITS_TO_STR[c0][RANK] + CARD_BITS_TO_STR[c0][SUITE]]
+        c1_1d = dict_str_to_sk[CARD_BITS_TO_STR[c1][RANK] + CARD_BITS_TO_STR[c1][SUITE]]
+        board = BOARD_BITS_TO_STR[board_mask.astype(bool)]
+        # board = array(['A', 'c', '2', 'h', '8', 'd'], dtype='<U1')
+        board_cards = []
+        for i in range(0, sum(board_mask) - 1, 2):  # sum is 6,8,10 for flop turn river resp.
+            board_cards.append(dict_str_to_sk[board[i] + board[i + 1]])
+
+        return [c0_1d, c1_1d], board_cards
+
+    def look_at_cards(self, obs: np.array) -> Tuple[List[int], List[int]]:
+        c0_bits = obs[IDX_C0_0:IDX_C0_1].astype(bool)
+        c1_bits = obs[IDX_C1_0:IDX_C1_1].astype(bool)
+        board_bits = obs[IDX_BOARD_START:IDX_BOARD_END].astype(int)  # bit representation
+        return self.card_bit_mask_to_int(c0_bits, c1_bits, board_bits)
+
+    def compute_action(self,
+                       obs: Union[List, np.ndarray]):
+        hero_cards_1d, board_cards_1d = self.look_at_cards(obs)
+        # from cards get winning probabilityx
+        mc_dict = self._card_evaluator.run_mc(hero_cards_1d, board_cards_1d, 2, n_iter=self._mc_iters)
+        # {won: 0, lost: 0, tied: 0}[
+        win_prob = float(mc_dict['won'] / self._mc_iters)
+        # todo: replace win_prob < .5 with EV based fn -- win_prob 20% requires min_to_call <= 1/5 pot
+        if win_prob < .5 and random() < self.tightness:
+            return torch.tensor(ActionSpace.FOLD.value)
+        else:
+            assert len(self._predictions == 1)
+            prediction = self._predictions[0]
+            # return raise of size at most the predicted size bucket
+            if min(int(prediction), 2) in self.next_legal_moves:
+                return prediction
+            elif ActionSpace.CHECK_CALL in self.next_legal_moves:
+                return torch.tensor(ActionSpace.CHECK_CALL.value)
+            else:
+                return torch.tensor(ActionSpace.FOLD.value)
+
+    def load_model(self):
+        input_dim = 564
+        classes = [ActionSpace.FOLD,
+                   ActionSpace.CHECK_CALL,  # CHECK IS INCLUDED in CHECK_CALL
+                   ActionSpace.RAISE_MIN_OR_3BB,
+                   ActionSpace.RAISE_HALF_POT,
+                   ActionSpace.RAISE_POT,
+                   ActionSpace.ALL_IN]
+        hidden_dim = [512, 512]
+        output_dim = len(classes)
+        net = MLP(input_dim, output_dim, hidden_dim)
+        # if running on GPU and we want to use cuda move model there
+        # use_cuda = torch.cuda.is_available()
+        # if use_cuda:
+        #     net = net.cuda()
+        self._model = net
+        os.environ['PRL_BASELINE_MODEL_PATH'] = "/home/sascha/Documents/github.com/prl_baselines/data/baseline_model_ckpt.pt"
+        self._model.load_state_dict(torch.load(os.environ['PRL_BASELINE_MODEL_PATH'],
+                                               # always on cpu because model used to collects rollouts
+                                               map_location=torch.device('cpu'))['net'])
+        self._model.eval()
+        return net
+
     def step(self, action):
         # todo add the following code
-        # if action == MultiAgentActionFlags.TriggerMC:
-        #     # compute action here
-        #     pass
+        if action == MultiAgentActionFlags.TriggerMC:
+            self._logits = self._model(torch.Tensor([self._last_obs]))
+            self._predictions = torch.argmax(self._logits, axis=1)
+            action = self.compute_action(self._last_obs)
         if (
                 self.terminations[self.agent_selection]
                 or self.truncations[self.agent_selection]
         ):
             return self._was_dead_step(action)
         obs, rew, done, info = self.env_wrapped.step(action)
+        self._last_obs = obs
         next_player_id = self.env_wrapped.current_player.seat_id
         next_player = self._int_to_name(next_player_id)
         if done:
@@ -167,7 +257,7 @@ class TianshouEnvWrapper(AECEnv):
             legal_moves = np.array([0, 0, 0, 0, 0, 0])
             legal_moves[self.env_wrapped.env.get_legal_actions()] += 1
             if legal_moves[2] == 1:
-                legal_moves[[3,4,5]] = 1
+                legal_moves[[3, 4, 5]] = 1
             self.next_legal_moves = legal_moves
         self._cumulative_rewards[self.agent_selection] = 0
         self.agent_selection = next_player
@@ -241,10 +331,23 @@ def get_rainbow_config():
             }
 
 
+class MCPolicy(BasePolicy):
+    def __init__(self, observation_space=None, action_space=None):
+        super().__init__(observation_space=observation_space,
+                         action_space=action_space)
+
+    def forward(self, batch: Batch, state: Optional[Union[dict, Batch, np.ndarray]] = None, **kwargs: Any) -> Batch:
+        nobs = len(batch.obs)
+        return Batch(logits=None, act=[MultiAgentActionFlags.TriggerMC] * nobs, state=None)
+
+    def learn(self, batch: Batch, **kwargs: Any) -> Dict[str, Any]:
+        pass
+
+
 rainbow_config = get_rainbow_config()
 policy = MultiAgentPolicyManager([
     RainbowPolicy(**rainbow_config),
-    RainbowPolicy(**rainbow_config)], wrapped_env)  # policy is made from PettingZooEnv
+    MCPolicy()], wrapped_env)  # policy is made from PettingZooEnv
 
 collector = Collector(policy, venv)
 result = collector.collect(n_episode=1)
