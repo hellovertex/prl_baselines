c904fa29522e2cbc8ea0d245d406ed182243aea0
sascha <hellovertex@outlook.com>
2023-01-30 02:27:09 +0100

    upd sync

--- prl/baselines/examples/example_mc_agent.py
+++ prl/baselines/examples/example_mc_agent.py
@@ -1,3 +1,6 @@
+import torch
+from torch import softmax
+
 from prl.baselines.agents.tianshou_agents import MCAgent
 import pandas as pd
 
@@ -8,8 +11,27 @@ df = pd.read_csv(fpath,
                  encoding='cp1252')
 legal_moves = [1, 1, 1, 1, 1, 1]  # 1 for each action
 agent = MCAgent()
-for i in range(len(df)):
+cummax = {0:0,
+          1:0,
+          2:0,
+          3:0,
+          4:0,
+          5:0,
+          6:0}
+n_obs = len(df)
+for i in range(n_obs):
+    print(f'Iterating row {i} of {n_obs}')
     row = df.iloc[i].to_numpy()
     obs = row[1:]
     label = row[0]
     pred = agent.compute_action(obs, legal_moves)
+    cummax[pred] += torch.max(softmax(agent._logits, dim=1)).detach().numpy().item()
+avg_acceptance = {}
+for k,v in cummax:
+    avg_acceptance[k] = v / n_obs
+print(f'Average acceptance level over {n_obs} observations is per actions: '
+      f'CHECK_CALL: {avg_acceptance[1]}\n'
+      f'MIN_RAISE: {avg_acceptance[2]}\n'
+      f'RAISE_HALF_POT: {avg_acceptance[3]}\n'
+      f'RAISE_POT: {avg_acceptance[4]}\n'
+      f'ALL_IN: {avg_acceptance[5]}\n')
