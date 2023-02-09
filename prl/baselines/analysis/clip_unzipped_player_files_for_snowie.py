799cb1bcc462b2995090ba80faf7057862513c5c
hellovertex <hellovertex@outlook.com>
2023-02-09 06:20:29 +0100

    clipping unzipped player txt files works now -- running pokersnowie export

--- prl/baselines/analysis/clip_unzipped_player_files_for_snowie.py
+++ prl/baselines/analysis/clip_unzipped_player_files_for_snowie.py
@@ -1,24 +1,36 @@
+import os
+import re
 from functools import partial
 
 import glob
 import time
 import multiprocessing
+from pathlib import Path
 
 
-def write_shortened_version_for_snowie_analysis(filename, n_hands, out_path):
+def write_shortened_version_for_snowie_analysis(filename, n_hands, path_out):
     # read file and write back first n_hands
+    with open(filename, 'r', encoding='utf-8') as f:  # pylint: disable=invalid-name,unspecified-encoding
+        hand_database = f.read()
+        hands_played = re.split(r'PokerStars Hand #', hand_database)[1:]
+    if not os.path.exists(path_out):
+        os.makedirs(os.path.abspath(path_out))
+    with open(os.path.join(path_out, Path(filename).stem)+".txt", 'a+') as f:
+        for e in hands_played[:n_hands]:
+            f.write("PokerStars Hand #" + e)
 
-    pass
 
 
 if __name__ == '__main__':
     # load player dirs via multiprocessing and write back stripped down files for pokersnowie eval
     # 1k showdown hands
 
-    unzipped_dir = "/home/hellovertex/Documents/github.com/prl_baselines/data/01_raw/0.25-0.50/player_data"
+    #unzipped_dir = "/home/hellovertex/Documents/github.com/prl_baselines/data/01_raw/0.25-0.50/player_data"
+    #unzipped_dir = "/home/hellovertex/Documents/github.com/hellovertex/prl_baselines/data/player_data"
+    unzipped_dir = "/home/hellovertex/Documents/github.com/hellovertex/prl_baselines/data/player_data_test"
     filenames = glob.glob(unzipped_dir.__str__() + '/**/*.txt', recursive=True)
-    out_path = './snowie_unzipped'
-    write_shortened_version_for_snowie_analysis(filenames[0], 1, out_path)
+    path_out = './snowie_unzipped'
+    write_shortened_version_for_snowie_analysis(filenames[0], 5, path_out)
     # reduce_to_hands = 1000
     # start = time.time()
     # p = multiprocessing.Pool()
