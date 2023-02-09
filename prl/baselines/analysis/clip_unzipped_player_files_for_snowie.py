517cf1598faa9960af9558f841bb73eceed919e0
hellovertex <hellovertex@outlook.com>
2023-02-09 06:21:38 +0100

    upd sync

--- prl/baselines/analysis/clip_unzipped_player_files_for_snowie.py
+++ prl/baselines/analysis/clip_unzipped_player_files_for_snowie.py
@@ -24,20 +24,23 @@ def write_shortened_version_for_snowie_analysis(filename, n_hands, path_out):
 if __name__ == '__main__':
     # load player dirs via multiprocessing and write back stripped down files for pokersnowie eval
     # 1k showdown hands
-
+    debug = False
     #unzipped_dir = "/home/hellovertex/Documents/github.com/prl_baselines/data/01_raw/0.25-0.50/player_data"
     #unzipped_dir = "/home/hellovertex/Documents/github.com/hellovertex/prl_baselines/data/player_data"
     unzipped_dir = "/home/hellovertex/Documents/github.com/hellovertex/prl_baselines/data/player_data_test"
     filenames = glob.glob(unzipped_dir.__str__() + '/**/*.txt', recursive=True)
     path_out = './snowie_unzipped'
-    write_shortened_version_for_snowie_analysis(filenames[0], 5, path_out)
-    # reduce_to_hands = 1000
-    # start = time.time()
-    # p = multiprocessing.Pool()
-    # t0 = time.time()
-    # clip_fn = partial(write_shortened_version_for_snowie_analysis, n_hands=reduce_to_hands)
-    # for x in p.imap_unordered(clip_fn, filenames):
-    #     print(x + f'. Took {time.time() - t0} seconds')
-    # print(f'Finished job after {time.time() - start} seconds.')
-    #
-    # p.close()
+
+    if debug:
+        write_shortened_version_for_snowie_analysis(filenames[0], 5, path_out)
+
+    reduce_to_hands = 1000
+    start = time.time()
+    p = multiprocessing.Pool()
+    t0 = time.time()
+    clip_fn = partial(write_shortened_version_for_snowie_analysis, n_hands=reduce_to_hands, path_out=path_out)
+    for x in p.imap_unordered(clip_fn, filenames):
+        print(x + f'. Took {time.time() - t0} seconds')
+    print(f'Finished job after {time.time() - start} seconds.')
+
+    p.close()
