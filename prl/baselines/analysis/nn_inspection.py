1ae2e89c104b7fbdd941459a58e2dbfa2d5ec90a
sascha <hellovertex@outlook.com>
2023-02-11 04:25:05 +0100

    refactored nn_inspection

--- prl/baselines/analysis/nn_inspection.py
+++ prl/baselines/analysis/nn_inspection.py
@@ -10,6 +10,7 @@ import os
 import time
 from functools import partial
 from pathlib import Path
+from typing import Dict, Union
 
 import matplotlib
 import numpy as np
@@ -26,9 +27,74 @@ from prl.baselines.analysis.core.nn_inspector import Inspector
 from prl.baselines.supervised_learning.data_acquisition.hsmithy_parser import HSmithyParser
 
 
-def plot_heatmap(label_logits: dict,
-                 label_counts: dict,
-                 path_out_png: str) -> pd.DataFrame:
+def plot_heatmap(input_dict,
+                 path_out_png):
+    means = input_dict['means']
+    l = input_dict['l']
+    rows = ["Fold", "Check/Call", "Raise3BB", "Raise6BB", "Raise10BB", "Raise20BB",
+            "Raise50BB", "RaiseALLIN"]
+
+    df = pd.DataFrame(means).T  # do we need , index=idx, columns=cols?
+
+    # plot the heatmap with annotations
+    flatui = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
+    my_cmap = ListedColormap(sns.color_palette(flatui).as_hex())
+    fig, (ax1, ax2) = plt.subplots(1, 2,
+                                   figsize=(16, 6),
+                                   gridspec_kw={'width_ratios': [3, 1]})
+
+    # sns.heatmap(df, annot=True, ax=ax1, cmap='coolwarm')
+    cm = sns.heatmap(df, annot=True, ax=ax1, cmap='viridis')
+    im = cm.collections[0]
+    rgba = im.to_rgba(.1)
+    # rgb = tuple(map(int, 255 * rgba[:3]))
+    hex_value = matplotlib.colors.rgb2hex(rgba, keep_alpha=True)
+    what = 'mis-' if 'false' in path_out_png else 'correctly '
+    ax1.set_title(f"Average probabilities of the network on {what}predicting label")
+    ax2.bar(df.columns,
+            l,
+            color=[hex_value for _ in range(8)])
+    ax2.set_xlabel("Which Action")
+    ax2.set_ylabel("Number of times label was present")
+    ax2.set_title("Number of actions taken")
+    df['Sum'] = l
+    df.columns = [f'Predicted {ActionSpace(i)}' for i in range(len(ActionSpace))] + ['Sum']
+    df.index = rows
+    # adjust the subplots to occupy equal space
+    fig.tight_layout()
+    # fpath = os.path.join(os.getcwd(), 'result')
+    # fpath = os.path.join(fpath, Path(path_out_png).stem)
+
+    if not os.path.exists(Path(path_out_png).parent):
+        os.makedirs(Path(path_out_png).parent)
+    plt.savefig(os.path.join(Path(path_out_png).parent,
+                             Path(path_out_png).stem),
+                bbox_inches='tight')
+    plt.show()
+    return df
+
+
+def flush(results, path_out, inspector):
+    pass
+    # df = results['means']
+    # if not os.path.exists(path_out):
+    #     os.makedirs(path_out)
+    # df.to_csv(f'{path_out}/false_probabs.csv')
+    # df = pd.DataFrame(inspector.label_counts_false,
+    #                   index=[k.name for k in list(inspector.label_counts_false.keys())])
+    # df.to_csv(f'{path_out}/false_labels.csv')
+    # print(df)
+    # # TRUE PREDICTIONS
+    # df = plot_heatmap(results,
+    #                   path_out_png=f'{path_out}/correct.png')
+    # df.to_csv(f'{path_out}/correct_probas.csv')
+    # df = pd.DataFrame(inspector.label_counts_true,
+    #                   index=[k.name for k in list(inspector.label_counts_true.keys())])
+    # df.to_csv(f'{path_out}/true_labels.csv')
+    # print(df)
+
+
+def compute(label_logits, label_counts) -> Dict[str, Union[torch.Tensor, Dict]]:
     means = {}
     maas = {}
     mins = {}
@@ -90,86 +156,14 @@ def plot_heatmap(label_logits: dict,
                'percentile_50': percentile_50,
                'percentile_75': percentile_75,
                'percentile_90': percentile_90,
+               'l': l  # todo rename
                }
-    rows = ["Fold", "Check/Call", "Raise3BB", "Raise6BB", "Raise10BB", "Raise20BB",
-            "Raise50BB", "RaiseALLIN"]
-
-    df = pd.DataFrame(means).T  # do we need , index=idx, columns=cols?
-
-    # plot the heatmap with annotations
-    flatui = ["#9b59b6", "#3498db", "#95a5a6", "#e74c3c", "#34495e", "#2ecc71"]
-    my_cmap = ListedColormap(sns.color_palette(flatui).as_hex())
-    fig, (ax1, ax2) = plt.subplots(1, 2,
-                                   figsize=(16, 6),
-                                   gridspec_kw={'width_ratios': [3, 1]})
-
-    # sns.heatmap(df, annot=True, ax=ax1, cmap='coolwarm')
-    cm = sns.heatmap(df, annot=True, ax=ax1, cmap='viridis')
-    im = cm.collections[0]
-    rgba = im.to_rgba(.1)
-    # rgb = tuple(map(int, 255 * rgba[:3]))
-    hex_value = matplotlib.colors.rgb2hex(rgba, keep_alpha=True)
-    what = 'mis-' if 'false' in path_out_png else 'correctly '
-    ax1.set_title(f"Average probabilities of the network on {what}predicting label")
-    ax2.bar(df.columns,
-            l,
-            color=[hex_value for _ in range(8)])
-    ax2.set_xlabel("Which Action")
-    ax2.set_ylabel("Number of times label was present")
-    ax2.set_title("Number of actions taken")
-    df['Sum'] = l
-    df.columns = [f'Predicted {ActionSpace(i)}' for i in range(len(ActionSpace))] + ['Sum']
-    df.index = rows
-    # adjust the subplots to occupy equal space
-    fig.tight_layout()
-    # fpath = os.path.join(os.getcwd(), 'result')
-    # fpath = os.path.join(fpath, Path(path_out_png).stem)
-
-    if not os.path.exists(Path(path_out_png).parent):
-        os.makedirs(Path(path_out_png).parent)
-    plt.savefig(os.path.join(Path(path_out_png).parent,
-                             Path(path_out_png).stem),
-                bbox_inches='tight')
-    plt.show()
 
     return results
 
 
-def make_results(inspector, path_out):
-    # WRONG PREDICTIONS
-    results = plot_heatmap(label_logits=inspector.logits_when_wrong,
-                      label_counts=inspector.label_counts_false,
-                      path_out_png=f'{path_out}/false.png')
-    df = results['means']
-    if not os.path.exists(path_out):
-        os.makedirs(path_out)
-    df.to_csv(f'{path_out}/false_probabs.csv')
-    df = pd.DataFrame(inspector.label_counts_false,
-                      index=[k.name for k in list(inspector.label_counts_false.keys())])
-    df.to_csv(f'{path_out}/false_labels.csv')
-    print(df)
-    # TRUE PREDICTIONS
-    df = plot_heatmap(label_logits=inspector.logits_when_correct,
-                      label_counts=inspector.label_counts_true,
-                      path_out_png=f'{path_out}/correct.png')
-    df.to_csv(f'{path_out}/correct_probas.csv')
-    df = pd.DataFrame(inspector.label_counts_true,
-                      index=[k.name for k in list(inspector.label_counts_true.keys())])
-    df.to_csv(f'{path_out}/true_labels.csv')
-    print(df)
-
-    # todo write back model inspection
-    # with open(f'model_inspection_{pname}.txt', 'a+') as f:
-    #     for stat in inspector.player_stats:
-    #         f.write(json.dumps(stat.to_dict()))
-
-    # return f"Success. Wrote stats to {f'stats_{pname}.txt'}"
-
-
-def inspection(filename,
-               model_ckpt_abs_path,
-               path_out,
-               max_files=5):
+def collect(filename,
+            model_ckpt_abs_path):
     parser = HSmithyParser()
 
     if type(model_ckpt_abs_path) == str or type(model_ckpt_abs_path) == Path:
@@ -196,12 +190,33 @@ def inspection(filename,
     for ihand, hand in enumerate(parsed_hands[:5000]):
         print(f'Inspecting model on hand {ihand} / {num_parsed_hands}')
         inspector.inspect_episode(hand, pname=pname)
+    return inspector
+
+
+def run(filename,
+        model_ckpt_abs_path,
+        path_out,
+        max_files=5):
+    # run neural network on .txt file dataset to compute labels and collect statistics
+    inspector = collect(filename, model_ckpt_abs_path)
+
+    # compute mean, abs-std, min, max, percentiles [10,25,50,75,90] of predictions
+    results_wrong = compute(inspector.logits_when_wrong,
+                            inspector.label_counts_false)
+    results_correct = compute(inspector.logits_when_correct,
+                              inspector.label_counts_true)
+    # plot heatmaps
+    plot_heatmap(results_wrong, path_out_png=path_out + '/wrong.png')
+    plot_heatmap(results_correct, path_out_png=path_out + '/correct.png')
+
+    # write files to disk using df.to_csv()
+    flush(results_wrong, path_out, inspector)
+    flush(results_correct, path_out, inspector)
     # plots logits against true labels and saves csv with result to disk
-    make_results(inspector, path_out + Path(filename).stem)
     return f"Succes. Wrote file to {path_out + '/' + Path(filename).stem}"
 
 
-if __name__ == "__main__":
+def start():
     # model_ckpt_abs_path = "/home/sascha/Documents/github.com/prl_baselines/prl/baselines/supervised_learning/training/from_selected_players/with_folds_div_1/with_folds/ckpt_dir/ilaviiitech_[512]_1e-06/ckpt.pt"
     # model_ckpt_abs_path = "/home/hellovertex/Documents/github.com/prl_baselines/prl/baselines/supervised_learning/training/from_all_players/with_folds_2NL_all_players/ckpt_dir_[512]_1e-06/ckpt.pt"
 
@@ -228,20 +243,24 @@ if __name__ == "__main__":
     # unzipped_dir = "/home/sascha/Documents/github.com/prl_baselines/data/01_raw/0.25-0.50/unzipped"
     filenames = glob.glob(unzipped_dir.__str__() + '/**/*.txt', recursive=True)
 
-    inspect_fn = partial(inspection,
-                         model_ckpt_abs_path=model_ckpt_abs_path,
-                         path_out=path_out,
-                         max_files=max_files)
+    run_fn = partial(run,
+                     model_ckpt_abs_path=model_ckpt_abs_path,
+                     path_out=path_out,
+                     max_files=max_files)
     if debug:
-        inspect_fn(filenames[0])
+        run_fn(filenames[0])
     else:
         start = time.time()
         p = multiprocessing.Pool()
         t0 = time.time()
-        for x in p.imap_unordered(inspect_fn, filenames):
+        for x in p.imap_unordered(run_fn, filenames):
             print(x + f'. Took {time.time() - t0} seconds')
         print(f'Finished job after {time.time() - start} seconds.')
         p.close()
+
+
+if __name__ == "__main__":
+    start()
 
 # if __name__ == "__main__":
 #     # model_ckpt_abs_path = "/home/sascha/Documents/github.com/prl_baselines/prl/baselines/supervised_learning/training/from_selected_players/with_folds_div_1/with_folds/ckpt_dir/ilaviiitech_[512]_1e-06/ckpt.pt"
--- prl/baselines/supervised_learning/v2/new_hsmithy_parser.py
+++ prl/baselines/supervised_learning/v2/new_hsmithy_parser.py
@@ -9,10 +9,11 @@ I need a data pipeline that is not unnecessarily complex
 import glob
 import re
 from dataclasses import dataclass
-from typing import Optional, List, Tuple
+from typing import Optional, List, Tuple, Dict, Union
+
+from prl.environment.Wrappers.base import ActionSpace
 
 from prl.baselines.supervised_learning.data_acquisition.core.encoder import Positions6Max
-from prl.baselines.supervised_learning.data_acquisition.core.parser import PokerEpisode
 
 
 # all the following functionality should be possible with only minimal parameterization (input_dir, output_dir, ...)
@@ -36,12 +37,105 @@ class Player:
     position: Optional[Positions6Max] = None
 
 
-class ParseHsmithyTextToPokerEpisode:
-    def __init__(self):
-        pass
+@dataclass
+class Action:
+    who: str
+    what: Union[Tuple, ActionSpace]
+    how_much: Optional[int]
 
-    def parse_hand(self, hand_str):
-        players = {}
+
+@dataclass
+class PokerEpisode:
+    players: List[Player]
+    blinds: Dict[str, int]  # player_name -> amount
+    actions: Dict[str, List[Action]]
+
+
+class ParseHsmithyTextToPokerEpisode:
+    def __init__(self,
+                 preflop_sep="*** HOLE CARDS ***",
+                 flop_sep="*** FLOP ***",
+                 turn_sep="*** TURN ***",
+                 river_sep="*** RIVER ***",
+                 summary_sep="*** SUMMARY ***"):
+        self.preflop_sep = preflop_sep
+        self.flop_sep = flop_sep
+        self.turn_sep = turn_sep
+        self.river_sep = river_sep
+        self.summary_sep = summary_sep
+
+    def strip_next_round(self, strip_round, episode_str):
+        return episode_str.split(strip_round)[0]
+
+    def split_at_round(self, round, episode_str):
+        try:
+            return episode_str.split(round)[1]
+        except IndexError:
+            # index 1 cannot be accessed -> there is no `round`
+            return ""
+
+    def rounds(self, current_episode: str) -> Dict[str, str]:
+        hole_cards = self.split_at_round(self.preflop_sep, current_episode)
+        flop = self.split_at_round(self.flop_sep, current_episode)
+        turn = self.split_at_round(self.turn_sep, current_episode)
+        river = self.split_at_round(self.river_sep, current_episode)
+
+        # strip each round from the other rounds, so we isolate them for stat updates
+        # flop, turn, river may be empty string, so we have to find out where to
+        # strip each round
+        # 1. strip hole cards
+        if flop:
+            next_round = self.flop_sep
+            hole_cards = self.strip_next_round(self.flop_sep, hole_cards)
+        else:
+            hole_cards = self.strip_next_round(self.summary_sep, hole_cards)
+        # 2. strip flop cards
+        if turn:
+            # split at flop and strip from turn onwards
+            flop = self.strip_next_round(self.turn_sep, flop)
+        else:
+            flop = self.strip_next_round(self.summary_sep, flop)
+        # 3. strip turn cards
+        if river:
+            # split at turn and strip from river onwards
+            turn = self.strip_next_round(self.river_sep, turn)
+        else:
+            turn = self.strip_next_round(self.summary_sep, turn)
+        # 4. strip river cards
+        river = self.strip_next_round(self.summary_sep, river)
+        summary \
+            = self.split_at_round(self.summary_sep, current_episode)
+
+        # Assertions
+        # PREFLOP
+        assert not self.flop_sep in hole_cards
+        assert not self.turn_sep in hole_cards
+        assert not self.river_sep in hole_cards
+        assert not self.summary_sep in hole_cards
+        # FLOP
+        assert not self.preflop_sep in flop
+        assert not self.turn_sep in flop
+        assert not self.river_sep in flop
+        assert not self.summary_sep in flop
+        # TURN
+        assert not self.preflop_sep in turn
+        assert not self.flop_sep in turn
+        assert not self.river_sep in turn
+        assert not self.summary_sep in turn
+        # RIVER
+        assert not self.preflop_sep in river
+        assert not self.flop_sep in river
+        assert not self.turn_sep in river
+        assert not self.summary_sep in river
+
+        return {'preflop': hole_cards,
+                'flop': flop,
+                'turn': turn,
+                'river': river,
+                'summary': summary}
+
+    def get_players_and_blinds(self, hand_str) -> Tuple[Dict[str, Player], Dict[str, Dict[str, int]]]:
+        players = {}  # don't make this ordered, better to rely on names
         blinds = {}
         table = hand_str.split("*** HOLE CARDS ***")[0]
         lines = table.split('\n')
@@ -50,28 +144,47 @@ class ParseHsmithyTextToPokerEpisode:
                 seat, player = line.split(':')
                 seat_num = seat[-1]
                 pname, stack = player.split('(')
-                pname = pname[1:-1]
+                pname: str = pname[1:-1]
                 stack = stack[1:]
                 stack = float(stack.split(' ')[0])
-                player = Player(name=pname, seat_num=seat_num, stack=round(stack * 100))
+                player = Player(name=pname,
+                                seat_num=seat_num,
+                                stack=round(stack * 100))
                 players[pname] = player
             elif 'posts small blind' in line:
-                sb_name = line.split(":")[0]
-                sb_amt  = round(float(line.split("$"))*100)
+                sb_name: str = line.split(":")[0]
+                sb_amt = round(float(line.split("$")) * 100)
                 players[sb_name].position = Positions6Max.SB
                 blinds['sb'] = {sb_name: sb_amt}
             elif 'posts big blind' in line:
-                sb_name = line.split(":")[0]
-                sb_amt  = round(float(line.split("$"))*100)
-                players[sb_name].position = Positions6Max.BB
-                blinds['bb'] = {sb_name: sb_amt}
+                bb_name: str = line.split(":")[0]
+                bb_amt = round(float(line.split("$")) * 100)
+                players[bb_name].position = Positions6Max.BB
+                blinds['bb'] = {bb_name: bb_amt}
+        num_players = len(players)
+        return players, blinds
 
-    def parse_file(self, f: str, out: str, filtered_players: Optional[Tuple[str]]) -> List[PokerEpisode]:
+    def parse_hand(self, hand_str):
+        players, blinds = self.get_players_and_blinds(hand_str)
+        actions_flop = None
+        action_turn = None
+        actions_river = None
+        actions_as_sequence = None
+        board_cards = None
+
+        return PokerEpisode()
+
+    def parse_file(self, f: str,
+                   out: str,
+                   filtered_players: Optional[Tuple[str]],
+                   only_showdowns: bool) -> List[PokerEpisode]:
         """
         :param f: Absolute path to .txt file containing human-readable hhsmithy-export.
         :param out: Absolute path to .txt file containing
         :param filtered_players: If provided, the result will only contain games where
         a player in `filtered_players` participated.
+        :param only_showdowns: If True, will only generate episodes that finished.
+        As a consequence, if set to false, this returns PokerEpisodes where no player hands are visible.
         """
         # if filtered_players is None:
         # instead of using regex (which is slow) we must do it manually
@@ -81,6 +194,7 @@ class ParseHsmithyTextToPokerEpisode:
             hands_played = re.split(r'PokerStars Hand #', hand_database)[1:]
             for hand in hands_played:
                 episodes.append(self.parse_hand(hand))
+        return episodes
 
 
 if __name__ == "__main__":
@@ -89,4 +203,4 @@ if __name__ == "__main__":
     filenames = glob.glob(unzipped_dir + "/**/*.txt", recursive=True)
     parser = ParseHsmithyTextToPokerEpisode()
     for filename in filenames:
-        parser.parse_file(filename, out_dir, None)
+        parser.parse_file(filename, out_dir, None, True)
