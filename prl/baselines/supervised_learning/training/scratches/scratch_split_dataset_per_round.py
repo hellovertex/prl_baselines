fd2e4bb17e6f9a0934b895bc99bc7b5e62bbd505
hellovertex <hellovertex@outlook.com>
2023-02-15 08:36:15 +0100

    upd sync while implementing xgboost per round

--- prl/baselines/supervised_learning/training/scratches/scratch_split_dataset_per_round.py
+++ prl/baselines/supervised_learning/training/scratches/scratch_split_dataset_per_round.py
@@ -1,9 +1,13 @@
+import os
+from pathlib import Path
+
 import pandas as pd
 import glob
 from prl.environment.Wrappers.augment import AugmentedObservationFeatureColumns as cols
 
+
 def main():
-    path_to_csv_files = "/home/hellovertex/Documents/github.com/prl_baselines/prl/baselines/supervised_learning/v2/dataset"
+    path_to_csv_files = "/home/hellovertex/Documents/github.com/prl_baselines/prl/baselines/supervised_learning/v2/top_100_only_wins_no_folds"
     files = glob.glob(path_to_csv_files + "/**/*.csv.bz2", recursive=True)
 
     df = pd.read_csv(files[0],
@@ -24,13 +28,20 @@ def main():
         tmp = tmp.apply(pd.to_numeric, downcast='integer', errors='coerce').dropna()
         df = pd.concat([df, tmp], ignore_index=True)
         print(f'Loaded file {i}/{n_files}...')
+    for round in [cols.Round_preflop, cols.Round_flop, cols.Round_turn, cols.Round_river]:
+        file_path = f'./top_100_only_wins_no_folds_per_round/{round.name}/data.csv.bz2'
+        if not os.path.exists(Path(file_path).parent):
+            os.makedirs(os.path.realpath(Path(file_path).parent), exist_ok=True)
+        tmp = df[df[cols.Round_preflop.name.lower()] == 1]
+        tmp.to_csv(file_path,
+                   index=True,
+                   header=True,
+                   index_label='label',
+                   mode='a',
+                   float_format='%.5f',
+                   compression='bz2'
+                   )
 
-    df_preflop = df[df[cols.Round_preflop.name] == 1]
-    df_flop = df[df[cols.Round_flop.name] == 1]
-    df_turn = df[df[cols.Round_turn.name] == 1]
-    df_river = df[df[cols.Round_river.name] == 1]
-    df = df.sample(frac=1)
-    a = 1
 
 if __name__ == "__main__":
     main()
--- prl/baselines/supervised_learning/training/scratches/xgboost_testrun.py
+++ prl/baselines/supervised_learning/training/scratches/xgboost_testrun.py
@@ -1,1 +1,91 @@
-import xgboost
+import os
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import torch
+import xgboost as xgb
+from tensorboardX import SummaryWriter
+
+
+class TensorBoardCallback(xgb.callback.TrainingCallback):
+    def __init__(self, experiment: str = None, data_name: str = None):
+        self.experiment = experiment or "logs"
+        self.data_name = data_name or "test"
+        self.datetime_ = datetime.now().strftime("%Y%m%d-%H%M%S")
+        self.log_dir = f"runs/{self.experiment}/{self.datetime_}"
+        self.train_writer = SummaryWriter(log_dir=os.path.join(self.log_dir, "train/"))
+        if self.data_name:
+            self.test_writer = SummaryWriter(
+                log_dir=os.path.join(self.log_dir, f"{self.data_name}/")
+            )
+
+    def after_iteration(
+        self, model, epoch: int, evals_log: xgb.callback.TrainingCallback.EvalsLog
+    ) -> bool:
+        if not evals_log:
+            return False
+
+        for data, metric in evals_log.items():
+            for metric_name, log in metric.items():
+                score = log[-1][0] if isinstance(log[-1], tuple) else log[-1]
+                if data == "train":
+                    self.train_writer.add_scalar(metric_name, score, epoch)
+                else:
+                    self.test_writer.add_scalar(metric_name, score, epoch)
+
+        return False
+
+
+if __name__ == '__main__':
+    filename = "/home/hellovertex/Documents/github.com/prl_baselines/prl/baselines/supervised_learning/training/scratches/top_100_only_wins_no_folds_per_round/Round_preflop/data.csv.bz2"
+    df = pd.read_csv(filename,
+                     # df = pd.read_csv(path_to_csv_files,
+                     sep=',',
+                     dtype='float32',
+                     chunksize=1000,
+                     # dtype='float16',
+                     encoding='cp1252',
+                     compression='bz2')
+    tmp = next(df).apply(pd.to_numeric, downcast='integer', errors='coerce').dropna()
+    # remove duplicate label column
+    tmp.pop('label')
+
+    # one hot encode button
+    one_hot_btn = pd.get_dummies(tmp['btn_idx'], prefix='btn_idx')
+    tmp = pd.concat([tmp, one_hot_btn], axis=1)
+    tmp.drop('btn_idx', axis=1, inplace=True)
+
+    # make train test split
+    xtest = tmp.sample(frac=0.2, axis=0)
+    xtrain = tmp.drop(index=xtest.index)
+
+    ytest = xtest.pop('label.1')
+    ytrain = xtrain.pop('label.1')
+
+    # make Dmats
+    #mat_train = xgb.DMatrix(xg_train, label=ytrain)
+    xg_train = xgb.DMatrix(xtrain, label=ytrain)
+    #mat_test = xgb.DMatrix(xg_test, label=ytest)
+    xg_test = xgb.DMatrix(xtest, label=ytest)
+
+    # setup parameters for xgboost
+    param = {}
+    # use softmax multi-class classification
+    param['objective'] = 'multi:softprob'  # 'multi:softmax'
+    # scale weight of positive examples
+    param['eta'] = 0.1
+    param['max_depth'] = 6
+    param['nthread'] = 4
+    param['num_class'] = 8
+
+    watchlist = [(xg_train, 'train'), (xg_test, 'test')]
+    num_round = 5
+    callbacks = [TensorBoardCallback()]
+    bst = xgb.train(param,
+                    xg_train,
+                    num_round,
+                    watchlist,
+                    callbacks=[TensorBoardCallback(experiment='exp_1', data_name='test')])
+    a = 1
+
