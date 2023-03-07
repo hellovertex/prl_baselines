52cef5a0296c112d37da0c2a1e09cbc48b9df50b
sascha <hellovertex@outlook.com>
2023-03-07 13:21:17 +0100

    upd sync

--- prl/baselines/supervised_learning/v2/datasets/preprocessed_data.py
+++ prl/baselines/supervised_learning/v2/datasets/preprocessed_data.py
@@ -76,7 +76,7 @@ class PreprocessedData:
 
             # maybe reduce action space
             if self.opt.action_space[0] is ActionSpaceMinimal:
-                df[df['label'] > 2] = 2
+                df[df['label'] > max(ActionSpaceMinimal)] = max(ActionSpaceMinimal)
             elif self.opt.action_space[0] is ActionSpace:
                 assert df['label'].min == min(ActionSpace).value
                 assert df['label'].max == max(ActionSpace).value
--- prl/baselines/supervised_learning/v2/tests/test_preprocessed_data.py
+++ prl/baselines/supervised_learning/v2/tests/test_preprocessed_data.py
@@ -4,9 +4,10 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
+from prl.environment.Wrappers.base import ActionSpace, ActionSpaceMinimal
 
 from prl.baselines.supervised_learning.v2.datasets.dataset_config import ActionGenOption, \
-    DatasetConfig
+    DatasetConfig, Stage
 from prl.baselines.supervised_learning.v2.datasets.preprocessed_data import \
     make_preprocessed_data_if_not_exists_already
 
@@ -35,11 +36,20 @@ def dataset_config():
     return dataset_config
 
 
-def test_preprocessor(dataset_config):
+@pytest.fixture
+def csv_files(dataset_config):
     make_preprocessed_data_if_not_exists_already(dataset_config,
                                                  use_multiprocessing=True)
-    csv_files = glob.glob(dataset_config.dir_vectorized_data + '/**/*.csv.bz2',
-                          recursive=True)
+    return glob.glob(dataset_config.dir_vectorized_data + '/**/*.csv.bz2',
+                     recursive=True)
+
+def assert_all_actions_present_and_no_other_actions_as_specified(df, action_space):
+    assert not df['label'] > max(action_space)
+    assert not df['label'] < min(action_space)
+    for a in list(action_space):
+        assert df['label'] == a.value
+def test_preprocessor_action_clipping(dataset_config, csv_files):
+    act = dataset_config.action_space[0]
     for file in csv_files:
         df = pd.read_csv(file,
                          sep=',',
@@ -47,3 +57,26 @@ def test_preprocessor(dataset_config):
                          encoding='cp1252',
                          compression='bz2')
         assert df['label'].max
+        if act is ActionSpace:
+            assert_all_actions_present_and_no_other_actions_as_specified(df, act)
+        elif act is ActionSpaceMinimal:
+            assert_all_actions_present_and_no_other_actions_as_specified(df, act)
+        elif isinstance(act, ActionSpaceMinimal):
+            assert_all_actions_present_and_no_other_actions_as_specified(df, act)
+
+
+def test_preprocessor_target_round_clipping(dataset_config, csv_files):
+    for file in csv_files:
+        df = pd.read_csv(file,
+                         sep=',',
+                         dtype='float32',
+                         encoding='cp1252',
+                         compression='bz2')
+        assert df['label'].max
+        for stage in dataset_config.target_round:
+            name = 'round_' + stage.name.casefold()
+            assert df[name] == 1
+        for stage in list(Stage):
+            if stage not in dataset_config.target_round:
+                name = 'round_' + stage.name.casefold()
+                assert not df[name] == 1
