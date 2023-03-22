705099502483189b6c68952a134cab370c30f6f7
sascha <hellovertex@outlook.com>
2023-03-22 01:47:45 +0100

    upd sync

--- prl/baselines/analysis/core/baseline_mbbg.py
+++ prl/baselines/analysis/core/baseline_mbbg.py
@@ -1,9 +1,12 @@
 # 1. load .ckpt file into baesline agent [x]
 # 2. run games and collect rewards
+import numpy as np
+from prl.environment.Wrappers.base import ActionSpace
+
 from prl.baselines.agents.tianshou_agents import ImitatorAgent
 from prl.baselines.evaluation.v2.eval_agent import EvalAgentTianshou, EvalAgentRandom
 from prl.baselines.examples.examples_tianshou_env import make_default_tianshou_env
-
+import matplotlib.pyplot as plt
 
 def load_imitation_agent(ckpt_dir):
     return ImitatorAgent(ckpt_dir,
@@ -11,9 +14,9 @@ def load_imitation_agent(ckpt_dir):
                          model_hidden_dims=(512,))
 
 
-def main(max_episodes, agents, agent_names, pname):
+def run(max_episodes, agents, agent_names, pname):
     # 1. make env
-    num_players = len(agents)
+    assert num_players == len(agents)
     path_out = "./baseline_stats"
     starting_stack = 5000
     env = make_default_tianshou_env(mc_model_ckpt_path=None,  # dont use mc
@@ -21,15 +24,35 @@ def main(max_episodes, agents, agent_names, pname):
                                                  range(len(agent_names))],
                                     agents=agent_names,
                                     num_players=len(agent_names))
-    obs = env.reset()
-    a = 1
-    # 2. run ${max_episodes} games
-    # while not done:
-    #     pass
-    # 3. collect games for ${pname}
+    targets_rewards = []
+    pidx = agent_names.index(pname)
+    for _ in range(max_episodes):
+        obs_dict = env.reset()
+        obs = obs_dict['obs']
+        agent_id = obs_dict['agent_id']
+        legal_moves = obs_dict['mask']
+        while True:
+            i = agent_names.index(agent_id)
+            action = agents[i].act([obs], legal_moves)
+            if obs_dict['mask'][8] == 1:
+                action = ActionSpace.NoOp
+            obs_dict, rews, terminated, truncated, info = env.step(action)
+            agent_id = obs_dict['agent_id']
+            obs = obs_dict['obs']
+            if terminated:
+                targets_rewards.append(rews[pidx])
+                break
+    mean_rews = np.mean(targets_rewards)
+    print(f'BB/H={mean_rews}, MBB/G={mean_rews * 1000}')
+    return targets_rewards
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
 if __name__ == '__main__':
-    max_episodes = 10
+    max_episodes = 1000
     num_players = 6
     pname = 'AI_AGENT_v2'
     agent_names = [f'{pname}', '2', '3', '4', '5', '6']
@@ -39,4 +62,17 @@ if __name__ == '__main__':
     agents = [EvalAgentTianshou(pname, agent)]
     agents += [EvalAgentRandom(f'p{i + 1}') for i in
                range(num_players - 1)]
-    main(max_episodes, agents, agent_names, pname)
+    rews = run(max_episodes, agents, agent_names, pname)
+    rews = np.cumsum(rews)
+    plt.xlabel('Hands played')
+    plt.ylabel('Big Blinds won')
+    plt.yaxis.set_label_position("right")
+    plt.set_cmap('viridis')
+    plt.grid()
+
+    plt.axhline(0, color='black')
+
+    plt.rcParams.update({'font.size': 12, 'font.family': 'Times New Roman'})
+    plt.title('Winnings in Big Blinds per Game')
+    plt.plot(np.arange(len(rews)), rews)
+    plt.show()
